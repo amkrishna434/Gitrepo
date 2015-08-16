b31648447d54a352f6bdf6bd722721b5e0f07e9a
amkrishna434 <ambatimurali@ymail.com>
2015-08-16 15:16:15 +0530

    modified data in dev_branch CallingMethodsInSameClass.java

--- CallingMethodsInSameClass.java
+++ CallingMethodsInSameClass.java
@@ -1,23 +1,15 @@
-/* CallingMethodsInSameClass.java
- *
- * illustrates how to call static methods a class
- * from a method in the same class
- */
-
 public class CallingMethodsInSameClass
 {
-	public static void main(String[] args) {
-		printOne();
-		printOne();
-		printTwo();
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
 	}
-
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
-
-	public static void printTwo() {
-		printOne();
-		printOne();
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
 	}
 }
