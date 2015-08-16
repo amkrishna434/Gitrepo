15a82d0e823767ba6fd2a9ed737444b04f4cfbee
amkrishna434 <ambatimurali@ymail.com>
2015-08-16 15:25:28 +0530

    modified HelloWorld.java file in dev_braanch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,24 @@
-public class HelloWorld
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+/*
+Java Hello World.
+*/
+ 
+public class HelloWorld{
+ 
+  public static void main(String args[]){
+ 
+    /*
+    Use System.out.println() to print on console.
+    */
+    System.out.println("Hello Murali !");
+ 
+  }
+ 
 }
+ 
+/*
+ 
+OUTPUT of the above given Java Hello World Example would be :
+ 
+Hello World !
+ 
+*/
